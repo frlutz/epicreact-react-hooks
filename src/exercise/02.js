df9a4e6d7fe87e2949758f7f0544990c10716526
Fredrik Lütz <fredde_lutz@hotmail.com>
2021-05-15 08:07:34 +0200

    exercise 2 extra credit 1

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,8 +4,9 @@
 import * as React from 'react'
 
 function Greeting({initialName = ''}) {
-  const initialNameState = window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialNameState)
+  const [name, setName] = React.useState(
+    () => window.localStorage.getItem('name') || initialName,
+  )
 
   React.useEffect(() => {
     window.localStorage.setItem('name', name)
