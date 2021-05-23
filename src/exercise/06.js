e86f5dabdfabdb9f0a3cbb9babb6230c1ad7f065
Fredrik Lütz <fredde_lutz@hotmail.com>
2021-05-23 12:47:23 +0200

    exercise 6 extra credit 8

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -57,6 +57,7 @@ function App() {
         <ErrorBoundary
           FallbackComponent={ErrorFallback}
           onReset={() => setPokemonName('')}
+          resetKeys={[pokemonName]}
         >
           <PokemonInfo pokemonName={pokemonName} />
         </ErrorBoundary>
